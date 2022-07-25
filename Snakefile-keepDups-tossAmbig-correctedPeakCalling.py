1a9440bddabb9fc9413b405bbbe75c0af0be5936
Alex Stutzman <14stutzmanav@gmail.com>
2022-07-25 12:52:07 -0400

    Added all to --keep-dup flag in callPeaks

--- Snakefile-keepDups-tossAmbig-correctedPeakCalling.py
+++ Snakefile-keepDups-tossAmbig-correctedPeakCalling.py
@@ -548,7 +548,7 @@ rule callPeaks:
 		modules['macsVer']
 	shell:
 		"""
-		macs2 callpeak -f BEDPE -c {params.control} -n {params.prefix} -g 121400000 -t {input} --nomodel --nolambda --seed 123 --keep-dup
+		macs2 callpeak -f BEDPE -c {params.control} -n {params.prefix} -g 121400000 -t {input} --nomodel --nolambda --seed 123 --keep-dup all
 		"""
 
 rule qcReport:
