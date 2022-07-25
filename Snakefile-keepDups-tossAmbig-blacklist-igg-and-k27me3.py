bb1d4e3568071f91034aff0013a3ab678a7fc84b
Alex Stutzman <14stutzmanav@gmail.com>
2022-07-25 16:09:49 -0400

    Removed control from callPeaks rule

--- Snakefile-keepDups-tossAmbig-blacklist-igg-and-k27me3.py
+++ Snakefile-keepDups-tossAmbig-blacklist-igg-and-k27me3.py
@@ -553,7 +553,7 @@ rule callPeaks:
 		modules['macsVer']
 	shell:
 		"""
-		macs2 callpeak -f BEDPE -c {params.control} -n {params.prefix} -g 121400000 -t {input} --nomodel --seed 123 --nolambda --keep-dup all
+		macs2 callpeak -f BEDPE -n {params.prefix} -g 121400000 -t {input} --nomodel --seed 123 --nolambda --keep-dup all
 		"""
 
 rule qcReport:
